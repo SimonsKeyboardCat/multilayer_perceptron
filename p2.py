7afc17cedf995f42824b6d40d7ba3e414a86e1a0
szymon_p <szymon.polanski24@gmail.com>
2024-05-08 23:01:00 +0200

    added acvation ReLU

--- p2.py
+++ p2.py
@@ -1,10 +1,15 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
 
+nnfs.init()
 np.random.seed(0)
 
 X = [[1, 2, 3, 2.5],
      [2.0, 5.0, -1.0, 2.0],
      [-1.5, 2.7, 3.3, -0.8]]
+
+X, y = spiral_data(100, 3)
 
 class Layer_Dense:
       def __init__(self, n_inputs, n_neurons):
@@ -13,11 +18,19 @@ class Layer_Dense:
       def forward(self, inputs):
          self.output = np.dot(inputs, self.weights) + self.biases
 
-layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)
+class Activaction_RreLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+layer1 = Layer_Dense(2, 5)
+activation1 = Activaction_RreLU()
+# layer2 = Layer_Dense(5, 2)
 layer1.forward(X)
-print(layer1.weights)
-print(layer1.biases)
 print(layer1.output)
-layer2.forward(layer1.output)
-print(layer2.output)
+activation1.forward(layer1.output)
+print(activation1.output)
+# print(layer1.weights)
+# print(layer1.biases)
+# print(layer1.output)
+# layer2.forward(layer1.output)
+# print(layer2.output)
