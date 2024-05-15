35067e6c70925e2ee8ec50fc982213069896bf0f
szymon_p <szymon.polanski24@gmail.com>
2024-05-15 15:36:19 +0200

    working forward and backward passes

--- p2.py
+++ p2.py
@@ -3,16 +3,15 @@ import nnfs
 from nnfs.datasets import spiral_data
 
 nnfs.init()
-np.random.seed(0)
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
       
     def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases
         self.inputs = inputs
+        self.output = np.dot(inputs, self.weights) + self.biases
 
     def backward(self, dvalues):
         self.dweights = np.dot(self.inputs.T, dvalues)
@@ -20,7 +19,7 @@ class Layer_Dense:
         self.dinputs = np.dot(dvalues, self.weights.T)
 
 
-class Activaction_ReLU:
+class Activation_ReLU:
     def forward(self, inputs):
         self.inputs = inputs
         self.output = np.maximum(0, inputs)
@@ -123,44 +122,48 @@ class Activation_Softmax_Loss_CategoricalCrossentropy():
         # Normalize gradient
         self.dinputs = self.dinputs / samples
 
-# X, y = spiral_data(samples=100, classes=3)
-
-# dense1 = Layer_Dense(2, 3)
-# activation1 = Activaction_ReLU()
-
-# dense2 = Layer_Dense(3, 3)
-# activation2 = Activation_Softmax()
-
-# dense1.forward(X)
-# activation1.forward(dense1.output)
-
-# dense2.forward(activation1.output)
-# activation2.forward(dense2.output)
-
-# print(activation2.output[:5])
-
-# loss_function = Loss_CategoricalCrossentropy()
-# loss = loss_function.calculate(activation2.output, y)
-
-# print('Loss:', loss)
-
-softmax_outputs = np.array([[0.7, 0.1, 0.2],
-                            [0.1, 0.5, 0.4],
-                            [0.02, 0.9, 0.08]])
-class_targets = np.array([0, 1, 1])
-softmax_loss = Activation_Softmax_Loss_CategoricalCrossentropy()
-softmax_loss.backward(softmax_outputs, class_targets)
-dvalues1 = softmax_loss.dinputs
-activation = Activation_Softmax()
-activation.output = softmax_outputs
-loss = Loss_CategoricalCrossentropy()
-loss.backward(softmax_outputs, class_targets)
-activation.backward(loss.dinputs)
-dvalues2 = activation.dinputs
-
-
-print('Gradients: combined loss and activation:')
-print(dvalues1)
-print('Gradients: separate loss and activation:')
-print(dvalues2)
+# Create dataset
+X, y = spiral_data(samples=100, classes=3)
+# Create Dense layer with 2 input features and 3 output values
+dense1 = Layer_Dense(2, 3)
+# Create ReLU activation (to be used with Dense layer):
+activation1 = Activation_ReLU()
+# Create second Dense layer with 3 input features (as we take output
+# of previous layer here) and 3 output values (output values)
+dense2 = Layer_Dense(3, 3)
+# Create Softmax classifier's combined loss and activation
+loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
+# Perform a forward pass of our training data through this layer
+dense1.forward(X)
+# Perform a forward pass through activation function
+# takes the output of first dense layer here
+activation1.forward(dense1.output)
+# Perform a forward pass through second Dense layer
+# takes outputs of activation function of first layer as inputs
+dense2.forward(activation1.output)
+# Perform a forward pass through the activation/loss function
+# takes the output of second dense layer here and returns loss
+loss = loss_activation.forward(dense2.output, y)
+# Let's see output of the first few samples:
+print(loss_activation.output[:5])
+# Print loss value
+print('loss:', loss)
+# Calculate accuracy from output of activation2 and targets
+# calculate values along first axis
+predictions = np.argmax(loss_activation.output, axis=1)
+if len(y.shape) == 2:
+    y = np.argmax(y, axis=1)
+accuracy = np.mean(predictions==y)
+# Print accuracy
+print('acc:', accuracy)
+# Backward pass
+loss_activation.backward(loss_activation.output, y)
+dense2.backward(loss_activation.dinputs)
+activation1.backward(dense2.dinputs)
+dense1.backward(activation1.dinputs)
+# Print gradients
+print(dense1.dweights)
+print(dense1.dbiases)
+print(dense2.dweights)
+print(dense2.dbiases)
 
