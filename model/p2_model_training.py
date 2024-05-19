7796f1911f3f79698070bc1e3ee063783a8fc8b1
szymon_p <szymon.polanski24@gmail.com>
2024-05-19 22:23:26 +0200

    added predictions

--- model/p2_model_training.py
+++ model/p2_model_training.py
@@ -410,47 +410,65 @@ class Accuracy_Categorical(Accuracy):
       return predictions == y
    
 
-# Label index to label name relation
-fashion_mnist_labels = {
-   0: 'T-shirt/top',
-   1: 'Trouser',
-   2: 'Pullover',
-   3: 'Dress',
-   4: 'Coat',
-   5: 'Sandal',
-   6: 'Shirt',
-   7: 'Sneaker',
-   8: 'Bag',
-   9: 'Ankle boot'
-}
-
-paths = [
-   "fashion_mnist_images/test/0/0100.png", 
-   "fashion_mnist_images/test/1/0100.png", 
-   "fashion_mnist_images/test/2/0100.png", 
-   "fashion_mnist_images/test/3/0100.png", 
-   "fashion_mnist_images/test/4/0100.png", 
-   "fashion_mnist_images/test/5/0100.png", 
-   "fashion_mnist_images/test/6/0100.png", 
-   "fashion_mnist_images/test/7/0100.png", 
-   "fashion_mnist_images/test/8/0100.png", 
-   "fashion_mnist_images/test/9/0100.png"
-   ]
-# Read an image
-image_data = cv2.imread(paths[1], cv2.IMREAD_GRAYSCALE)
-# Resize to the same size as Fashion MNIST images
-image_data = cv2.resize(image_data, (28, 28))
-# Invert image colors
-#image_data = 255 - image_data #this line is needed only for predictions folder
-# Reshape and scale pixel data
-image_data = (image_data.reshape(1, -1).astype(np.float32) -
-   127.5) / 127.5
+# Create dataset
+X, y, X_test, y_test = create_data_mnist('fashion_mnist_images')
+# Shuffle the training dataset
+keys = np.array(range(X.shape[0]))
+np.random.shuffle(keys)
+X = X[keys]
+y = y[keys]
+# Scale and reshape samples
+X = (X.reshape(X.shape[0], -1).astype(np.float32) - 127.5) / 127.5
+X_test = (X_test.reshape(X_test.shape[0], -1).astype(np.float32) -
+127.5) / 127.5
 # Load the model
 model = Model.load('fashion_mnist.model')
-# Predict on the image
-confidences = model.predict(image_data)
-# Get prediction instead of confidence levels
-predictions = model.output_layer_activation.predictions(confidences)
-# Get label name from label index
-prediction = fashion_mnist_labels[predictions[0]]
-print(prediction)
+# Evaluate the model
+confidences = model.predict(X_test[:5])
+print(confidences)
+# Instantiate the model
+model = Model()
+# Add layers
+model.add(Layer_Dense(X.shape[1], 128))
+model.add(Activation_ReLU())
+model.add(Layer_Dense(128, 128))
+model.add(Activation_ReLU())
+model.add(Layer_Dense(128, 10))
+model.add(Activation_Softmax())
+
+# Set loss and optimizer objects
+model.set(
+   loss=Loss_CategoricalCrossentropy(),
+   optimizer=Optimizer_Adam(decay=1e-4),
+   accuracy=Accuracy_Categorical()
+)
+
+model.finalize()
+
+model.train(X, y, validation_data=(X_test, y_test), epochs=10, batch_size=128, print_every=100)
+parameters = model.get_parameters()
+model.load('fashion_mnist.model')
+
+# New model
+# Instantiate the model
+model = Model()
+# Add layers
+model.add(Layer_Dense(X.shape[1], 128))
+model.add(Activation_ReLU())
+model.add(Layer_Dense(128, 128))
+model.add(Activation_ReLU())
+model.add(Layer_Dense(128, 10))
+model.add(Activation_Softmax())
+# Set loss and accuracy objects
+# We do not set optimizer object this time - there's no need to do it
+# as we won't train the model
+model.set(
+loss=Loss_CategoricalCrossentropy(),
+accuracy=Accuracy_Categorical()
+)
+# Finalize the model
+model.finalize()
+# Set model with parameters instead of training it
+model.load_parameters('fashion_mnist.parms')
+# Evaluate the model
+model.evaluate(X_test, y_test)
